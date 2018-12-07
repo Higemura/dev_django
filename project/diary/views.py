5fc0c5fab7440402eb617a3ca1f929fc0aaa9aaf
kitamura-k <kitamura-k@gnavi.co.jp>
2018-12-07 18:47:26 +0900

    次へボタン機能追加 管理画面追加

--- project/diary/views.py
+++ project/diary/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin # LoginRequiredMixinを継承させることでログイン必須にすることができる
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views import generic
@@ -9,22 +10,23 @@ from .models import Day
 class IndexView(generic.ListView): #generic.ListViewを継承したclassを作成
   model = Day
   # template_name = 'diary/my_list.html' これでも可だが別途 my_list.htmlを作成する必要がある
+  paginate_by = 3
 
 
-class AddView(generic.CreateView):
+class AddView(LoginRequiredMixin, generic.CreateView): # LoginRequiredMixinがついているのでログインしていないとこのclassわ使えない
   model = Day
   form_class = DayCreateForm
   # fields = '__all__' #単縦な汎用viewならform.pyが不要な為はこれでもOK
   success_url = reverse_lazy('diary:index') #データ追加が成功した時のリダイレクト先
 
 
-class UpdateView(generic.UpdateView):
+class UpdateView(LoginRequiredMixin, generic.UpdateView):
   model = Day
   form_class = DayCreateForm
   success_url = reverse_lazy('diary:index')
 
 
-class DeleteView(generic.DeleteView):
+class DeleteView(LoginRequiredMixin, generic.DeleteView):
   model = Day
   success_url = reverse_lazy('diary:index')
 
